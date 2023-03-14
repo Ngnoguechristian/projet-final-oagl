8a97c64b20082ddfe198655865eada9c3f240fef
Ngnoguechristian <89199263+Ngnoguechristian@users.noreply.github.com>
2023-03-14 08:42:05 +0100

    creation du login 1

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('welcome bro')
+from tkinter import *
+
+from pages.connexion.login import Login
+
+windows = Tk()
+# dinmensionner sa fenetre
+windows.geometry("1000x600+200+40")
+
+# changer le titre de la fenetre
+windows.title('Gestion de taches')
+
+# zone des pages de l'app
+page = Canvas(windows,width=1000,height=600)
+
+# transition pour le login
+Login(page,1000,600)
+
+page.place(x=0,y=0)
+
+# afficher la fenetre
+windows.mainloop()
---
+++ pages/connexion/login.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+
+def Login(windows,width, height):
+    page_login = Canvas(windows,height=height, width=width)
+
+    # titre de la page
+    Label(page_login,text="Login",font=('arial',20,'bold')).place(x=50,y=50)
+
+
+
+
+
+
+    page_login.place(x=0,y=0)
